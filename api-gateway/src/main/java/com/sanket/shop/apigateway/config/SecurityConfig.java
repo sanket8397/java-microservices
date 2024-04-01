e8de3b6be1e2fca691d6cb9deb0a4afdbd74676a
Sanket Kapse <sanket8397@gmail.com>
2024-04-01 16:55:43 -0700

    Spring Security Set up

--- api-gateway/src/main/java/com/sanket/shop/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/sanket/shop/apigateway/config/SecurityConfig.java
@@ -3,12 +3,12 @@ package com.sanket.shop.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
